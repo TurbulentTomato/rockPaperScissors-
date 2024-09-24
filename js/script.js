44576e0a6c57ae3f6f5a2ad4d39753a876be3f8c
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-09-24 10:50:06 +0530

    Add function to play game

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const display = document.querySelector(".display");
 const validMoves = ["rock", "paper", "scissors"];
 let compChoice = "";
 let playerChoice = "";
+let playerScore = 0;
+let compScore = 0;
 
 buttons.forEach(button => {
-  button.addEventListener("click", getPlayerChoice)
+  button.addEventListener("click", playGame)
 });
 
 function getComputerChoice() {
@@ -14,6 +16,54 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice(event) {
-  playerChoice = event.target.value.toLowerCase();
+  playerChoice = event.target.value;
+}
+
+function playGame(event) {
+  getComputerChoice();
+  getPlayerChoice(event);
+  let winner = ""
+  if (playerChoice === compChoice) {
+    winner = "No one"
+  }
+  else {
+    switch (playerChoice) {
+      case "rock":
+        if (compChoice === "paper") {
+          winner = "Computer";
+          compScore++;
+          break;
+        }
+      //if compChoice == sisccors, 
+      //breakthrough will happen and we get desired result
+      case "paper":
+        if (compChoice === "rock") {
+          winner = "Player";
+          playerScore++;
+          break;
+        }
+      //again using breakthrough
+      case "scissors":
+        if (compChoice === "rock") {
+          winner = "Computer";
+          compScore++;
+          break;
+        } else {
+          winner = "Player";
+          playerScore++;
+          break;
+        }
+      default:
+        message = "error";
+    }
+  }
+  let message = `
+Computer Played: ${compChoice} <br>
+Player Played: ${playerChoice} <br>
+${winner} wins!!! <br>
+Player Score: ${playerScore} <br>
+Computer Score: ${compScore}
+`
+  display.innerHTML = message;
 }
 
