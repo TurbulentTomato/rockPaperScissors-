7b4dc2522228bb335d4cae595f765add054f07cc
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-09-24 08:01:13 +0530

    Add function to get Player choice

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ const validMoves = ["rock", "paper", "scissors"];
 let compChoice = "";
 let playerChoice = "";
 
+buttons.forEach(button => {
+  button.addEventListener("click", getPlayerChoice)
+});
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   compChoice = validMoves[randomNumber];
 }
+
+function getPlayerChoice(event) {
+  playerChoice = event.target.value.toLowerCase();
+}
+
